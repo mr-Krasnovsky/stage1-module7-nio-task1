92ed71c9cea61f546fb5fba7efe737fd97b8f89f
Maxim Krasnov <mr.krasnovsky@gmail.com>
2023-05-10 23:38:01 +0300

    complited the solution

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,60 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        Profile newProfile = new Profile();
+
+        try (FileInputStream fis = new FileInputStream(file)){
+            int i;
+            String data = "";
+            while ((i =fis.read()) != -1){
+            data += (char) i;
+            }
+
+            newProfile = createProfile(data);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        return newProfile;
+    }
+
+    public Map<String, String> getProfileMap(String data) {
+        Map<String, String> profileMap = new LinkedHashMap<>();
+        String[] dataArray = data.split("\r\n");
+            for (String str: dataArray){
+                String [] profileString = str.split(": ");
+                profileMap.put(profileString[0],profileString[1]);
+            }
+            return profileMap;
+    }
+
+    public Profile createProfile(String data) {
+        Profile newProfile = new Profile();
+        Map<String, String> profileMap = getProfileMap(data);
+        for(Map.Entry<String, String> entry: profileMap.entrySet()){
+            switch (entry.getKey()){
+                case "Name":
+                    newProfile.setName(entry.getValue());
+                    break;
+                case "Age":
+                    newProfile.setAge(Integer.getInteger(entry.getValue()));
+                    break;
+                case "Email":
+                    newProfile.setEmail(entry.getValue());
+                    break;
+                case "Phone":
+                    newProfile.setPhone(Long.getLong(entry.getValue()));
+            }
+        }
+        return newProfile;
     }
 }
+
